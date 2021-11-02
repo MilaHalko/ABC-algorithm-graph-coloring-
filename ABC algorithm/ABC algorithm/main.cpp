e6b8e19844aa32d8e918012bf3c2a6f74ed6ea45
milka_deluxe <milagalko1@gmail.com>
2021-11-02 23:34:47 +0200

    ABC iteration function

--- ABC algorithm/ABC algorithm/main.cpp
+++ ABC algorithm/ABC algorithm/main.cpp
@@ -4,6 +4,8 @@
 using namespace::std;
 const int VERTEXES = 10;
 const int MIN_VERTEX = 2, MAX_VERTEX = 10;
+const int onlookerBees = 32;
+int employedBees = 3;
 
 void GraphGeneration(vector<vector<bool>> &graph) {
     graph.resize(VERTEXES);
@@ -22,11 +24,11 @@ void GraphGeneration(vector<vector<bool>> &graph) {
             graph[randVertex][i] = true;
         }
     }
-    
+    /*
     for (auto line : graph) {
         for (auto vertex: line) {cout << vertex << "  ";}
         cout << endl;
-    }
+    } */
 }
 
 class Vertex {
@@ -39,12 +41,28 @@ public:
     int getNectar() {return nectar;}
 };
 
+int coloring(vector<Vertex> vertexes, vector<vector<bool>> graph) {
+    int colorNums = 0;
+    
+    return colorNums;
+}
+
 void ABC(vector<vector<bool>> graph) {
+    // creating vertexes with it's degree info
     vector<Vertex> vertexes (VERTEXES);
     for (int i = 0; i < VERTEXES; i++) {
         int degree = 0;
         for (int j = 0; j < VERTEXES; j++) {{graph[i][j] ? degree++ : degree += 0;}}
         vertexes[i] = Vertex(degree);
+    }
+    
+    // iteration process
+    int chromaticNum = coloring(vertexes, graph);
+    int bestChromaticNum = chromaticNum;
+    for (int i = 2; i <= 1000; i++) {
+        chromaticNum = coloring(vertexes, graph);
+        bestChromaticNum = bestChromaticNum > chromaticNum ? chromaticNum : bestChromaticNum;
+        if (i % 20 == 0) {cout << i <<") Best chromatic number: " << bestChromaticNum << endl;}
     }
 }
 
